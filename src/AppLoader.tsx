b9b0823028ad5d513aee5aef0b403ef6b0e3a904
Liad Zigdon <liadzigdon@Liads-MBP.lan>
2021-10-02 21:34:54 +0300

    fix

--- src/AppLoader.tsx
+++ src/AppLoader.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline, StylesProvider, ThemeProvider } from "@material-ui/core";
 import "FontsLoader.css";
 import Container from "react-modal-promise";
-import { BrowserRouter as Router, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { useTitle } from "react-use";
 import { AppRouter } from "routing/AppRouter";
 import { HOME_ROUTE } from "routing/routes";
